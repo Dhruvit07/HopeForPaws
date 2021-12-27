92713ed08db62f30823b13f33ca930d53b76d955
Alok Rathava <alokrathava@gmail.com>
2021-12-27 08:03:34 -0500

    Initial commit

---
+++ footer.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Hope For Paws an Animals and Pets</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="keywords" content="Best Pets Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+	SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design"/>
+    <script type="application/x-javascript"> addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        } </script>
+    <!-- Custom Theme files -->
+    <link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="all">
+    <link href="css/font-awesome.css" rel="stylesheet"> <!-- font-awesome icons -->
+    <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property=""/>
+    <!-- //Custom Theme files -->
+    <!-- web-fonts -->
+    <link href="//fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Dosis:200,300,400,500,600,700,800" rel="stylesheet">
+    <!-- //web-fonts -->
+</head>
+<body>
+<!-- footer -->
+<div class="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="footer-grids">
+                <div class="col-md-3 col-sm-6 footer-logo">
+                    <div class="agileits-logo">
+                        <h2><a href="index.php">Best Pets</a></h2>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget egestas erat. In hac habitasse
+                        platea dictumst.</p>
+                </div>
+                <div class="col-md-3 col-sm-6 footer-grid">
+                    <h3>Navigation</h3>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="gallery.php">Gallery</a></li>
+                        <li><a href="icons.php">Web Icons</a></li>
+                        <li><a href="codes.php">Short Codes</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-6 footer-list">
+                    <h3>Latest Posts</h3>
+                    <ul>
+                        <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Vestibulum ante ipsum</li>
+                        <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Phasellus at eros</li>
+                        <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Mauris eleifend aliquet</li>
+                        <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Aliquam vitae tristique</li>
+                        <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Pellentesque lobortis</li>
+                        <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Class aptent taciti</li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-6 footer-grid">
+                    <h3>Twitter Posts</h3>
+                    <ul class="w3agile_footer_grid_list">
+                        <li>Ut aut reiciendis voluptatibus <a href="#">http://example.com</a> alias, ut aut.
+                            <span><i class="fa fa-twitter" aria-hidden="true"></i> 02 days ago</span></li>
+                        <li>Itaque earum rerum hic tenetur a sapiente <a href="#">http://example.com</a> ut aut.
+                            <span><i class="fa fa-twitter" aria-hidden="true"></i> 03 days ago</span></li>
+                    </ul>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- //footer -->
+<!-- modal sign in  -->
+<div class="modal bnr-modal about-modal w3-agileits fade" id="myModal2" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body login-page "><!-- login-page -->
+                <div class="sap_tabs">
+                    <div id="horizontalTab" style="display: block; width: 100%; margin: 0px;">
+                        <ul class="resp-tabs-list">
+                            <li class="resp-tab-item" aria-controls="tab_item-0"><span>Login</span></li>
+                            <li class="resp-tab-item" aria-controls="tab_item-1"><span>Register</span></li>
+                        </ul>
+                        <div class="clearfix"></div>
+                        <div class="resp-tabs-container">
+                            <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-0">
+                                <div class="agileits-login">
+                                    <form action="#" method="post">
+                                        <input type="email" class="email" name="Email" placeholder="Email" required=""/>
+                                        <input type="password" class="password" name="Password" placeholder="Password"
+                                               required=""/>
+                                        <div class="wthree-text">
+                                            <ul>
+                                                <li>
+                                                    <label class="anim">
+                                                        <input type="checkbox" class="checkbox">
+                                                        <span> Remember me ?</span>
+                                                    </label>
+                                                </li>
+                                                <li><a href="#">Forgot password?</a></li>
+                                            </ul>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                        <div class="w3ls-submit">
+                                            <input type="submit" value="LOGIN">
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-1">
+                                <div class="login-top sign-top">
+                                    <div class="agileits-login">
+                                        <form action="#" method="post">
+                                            <input type="text" name="Name" placeholder="Full Name" required="">
+                                            <input type="email" class="email" name="Email" placeholder="Email"
+                                                   required=""/>
+                                            <input type="password" class="password" name="Password"
+                                                   placeholder="Password" required=""/>
+                                            <label class="anim">
+                                                <input type="checkbox" class="checkbox">
+                                                <span> I accept the terms of use</span>
+                                            </label>
+                                            <div class="w3ls-submit">
+                                                <input class="register" type="submit" value="REGISTER">
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div> <!-- //login-page -->
+        </div>
+    </div>
+</div>
+<!-- //modal sign in -->
+<!-- js -->
+<script src="js/jquery-2.2.3.min.js"></script>
+<!-- //js -->
+<!-- jarallax -->
+<script src="js/SmoothScroll.min.js"></script>
+<script src="js/jarallax.js"></script>
+<script type="text/javascript">
+    /* init Jarallax */
+    $('.jarallax').jarallax({
+        speed: 0.5,
+        imgWidth: 1366,
+        imgHeight: 768
+    })
+</script>
+<!-- //jarallax -->
+<!-- flexSlider -->
+<script defer src="js/jquery.flexslider.js"></script>
+<script type="text/javascript">
+    $(window).load(function () {
+        $('.flexslider').flexslider({
+            animation: "slide",
+            start: function (slider) {
+                $('body').removeClass('loading');
+            }
+        });
+    });
+</script>
+<!-- //flexSlider -->
+<!-- start-smooth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+    jQuery(document).ready(function ($) {
+        $(".scroll").click(function (event) {
+            event.preventDefault();
+
+            $('html,body').animate({scrollTop: $(this.hash).offset().top}, 1000);
+        });
+    });
+</script>
+<!-- //end-smooth-scrolling -->
+<!-- smooth-scrolling-of-move-up -->
+<script type="text/javascript">
+    $(document).ready(function () {
+        /*
+        var defaults = {
+            containerID: 'toTop', // fading element id
+            containerHoverID: 'toTopHover', // fading element hover id
+            scrollSpeed: 1200,
+            easingType: 'linear'
+        };
+        */
+
+        $().UItoTop({easingType: 'easeOutQuart'});
+
+    });
+</script>
+<!-- //smooth-scrolling-of-move-up -->
+<!-- ResponsiveTabs js -->
+<script src="js/easyResponsiveTabs.js" type="text/javascript"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#horizontalTab').easyResponsiveTabs({
+            type: 'default', //Types: default, vertical, accordion
+            width: 'auto', //auto or any width like 600px
+            fit: true   // 100% fit in a container
+        });
+    });
+</script>
+<!-- //ResponsiveTabs js -->
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
