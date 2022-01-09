9dddfc3d9a69ac73b17cc75d31488f252f82935f
Alok Rathava <alokrathava@gmail.com>
2022-01-09 13:55:27 -0500

    Login Completed

--- admin/leftmenu.php
+++ admin/leftmenu.php
@@ -1,13 +1,13 @@
 <?php
-
 require 'includes/connect.php';
 
-//session_start();
-//if (!isset($_SESSION['a_id'])) {
-//    echo '<script>window.location.href="login.php"</script>';
-//}
-?>
+session_start();
 
+if (isset($_SESSION['a_id'])) {
+    echo '<script>window.location.href="login.php"</script>';
+}
+//$a_name = $_SESSION["a_name"];
+$a_name = "Admin";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,72 +29,49 @@ require 'includes/connect.php';
         <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
             <div class="sb-sidenav-menu">
                 <div class="nav">
-                    <div class="sb-sidenav-menu-heading">Core</div>
                     <a class="nav-link" href="index.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                         Dashboard
                     </a>
-                    <div class="sb-sidenav-menu-heading">Interface</div>
+                    <div class="sb-sidenav-menu-heading">E-Store</div>
                     <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts"
                        aria-expanded="false" aria-controls="collapseLayouts">
                         <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                        Layouts
+                        Product
                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                     </a>
                     <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne"
                          data-bs-parent="#sidenavAccordion">
                         <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="layout-static.php">Static Navigation</a>
-                            <a class="nav-link" href="layout-sidenav-light.php">Light Sidenav</a>
+                            <a class="nav-link" href="addCategories.php">Add Category</a>
+                            <a class="nav-link" href="addProduct.php">Add Product</a>
                         </nav>
                     </div>
                     <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages"
                        aria-expanded="false" aria-controls="collapsePages">
                         <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                        Pages
+                        Adoption
                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                     </a>
                     <div class="collapse" id="collapsePages" aria-labelledby="headingTwo"
                          data-bs-parent="#sidenavAccordion">
                         <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                            <a class="nav-link collapsed" href="adoption.php" data-bs-toggle="collapse"
                                data-bs-target="#pagesCollapseAuth" aria-expanded="false"
                                aria-controls="pagesCollapseAuth">
-                                Authentication
+                                Adoption List
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
-                            <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne"
-                                 data-bs-parent="#sidenavAccordionPages">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="login.php">Login</a>
-                                    <a class="nav-link" href="register.php">Register</a>
-                                    <a class="nav-link" href="password.php">Forgot Password</a>
-                                </nav>
-                            </div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
-                               data-bs-target="#pagesCollapseError" aria-expanded="false"
-                               aria-controls="pagesCollapseError">
-                                Error
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne"
-                                 data-bs-parent="#sidenavAccordionPages">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="401.php">401 Page</a>
-                                    <a class="nav-link" href="404.php">404 Page</a>
-                                    <a class="nav-link" href="500.php">500 Page</a>
-                                </nav>
-                            </div>
                         </nav>
                     </div>
-                    <div class="sb-sidenav-menu-heading">Addons</div>
-                    <a class="nav-link" href="charts.php">
+                    <div class="sb-sidenav-menu-heading">Add Ons</div>
+                    <a class="nav-link" href="complaints.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                        Charts
+                        User Complaints
                     </a>
-                    <a class="nav-link" href="tables.php">
+                    <a class="nav-link" href="payment.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                        Tables
+                        Payment
                     </a>
                     <div class="sb-sidenav-menu-heading">Logout</div>
                     <a class="nav-link" href="logout.php">
@@ -103,7 +80,7 @@ require 'includes/connect.php';
                 </div>
             </div>
             <div class="sb-sidenav-footer">
-                <div class="small">Logged in as: <?php $_SESSION['a_name']; ?></div>
+                <div class="small">Logged in as: <?php echo $a_name; ?></div>
             </div>
         </nav>
     </div>
---
+++ admin/login_process.php
@@ -0,0 +1,27 @@
+<?php
+require 'includes/connect.php';
+
+session_start();
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$loginSQL = "SELECT * FROM `admin` WHERE a_email='$email' AND a_password='$password'";
+
+$result = mysqli_query($conn, $loginSQL);
+
+if (mysqli_num_rows($result) == 1) {
+    while ($row = $result->fetch_assoc()) {
+        $a_id = $row['a_id'];
+        $a_name = $row['a_name'];
+    }
+    $_SESSION["a_name"] = $a_name;
+    $_SESSION["a_id"] = $a_id;
+
+    echo '<script>window.location.href="index.php"</script>';
+} else {
+    echo '<script>alert("Login Failed")</script>';
+    echo '<script>window.location.href="index.php"</script>';
+}
+
+?>
